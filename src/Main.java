b42b3bc539f6cafcd1917a2e36cce875b93994d3
vozle <vozle2@gmail.com>
2022-02-04 19:04:22 +0300

    Init

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketCost = 1337;
+        int mileCost = 20;
+
+        int bonusMile = ticketCost / mileCost;
+
+        System.out.println("Здравствуйте!");
+        System.out.println("Количество бонусных миль: " + bonusMile);
+
+    }
+}
